0a7696a2be214c4843b58ddcc4859b9a1e09af4f
Daniel <danicek76@yahoo.com>
2021-02-10 11:09:25 +0100

    initial upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveCalsses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveCalsses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
